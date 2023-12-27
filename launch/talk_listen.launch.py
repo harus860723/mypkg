b69d3879a282f920f092056dda132cdaadbb230d
harus <cleary.yokot@gmail.com>
2023-12-27 14:23:13 +0900

    launchの変更

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,11 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Haruki Matsushita
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
-  
-  
+
 def generate_launch_description():
-  
+
      talker = launch_ros.actions.Node(
          package='mypkg',
          executable='talker',
